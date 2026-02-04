f6e3d3b4dc418c14fd7825ac34b3ea3415004062
Sagor8187 <sdsagor8187@gmail.com>
2026-02-04 19:54:21 +0600

    now complete my task 5

--- task5.js
+++ task5.js
@@ -1,9 +1,21 @@
 // Task -5: (Hard)
 // For each employee their current salary is calculated by multiplying yearly increment with experience then adding the result to the starting salary. Now calculate is the total salary has to be provided by the company in a month.
 
-//  const employees = [
-//             { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
-//             { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
-//             { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
-//             { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
-//         ];
+ const employees = [
+            { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+            { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+            { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+            { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+        ];
+
+
+function calculate(employees){
+    // console.log(employees)
+    for(let employee of employees){
+        // console.log(employee)
+        let total = employee["experience"] * employee["increment"] + employee["starting"]
+        console.log(`name : ${employee["name"]} -> curent salary : ${total}` )
+    }
+}
+
+calculate(employees)
